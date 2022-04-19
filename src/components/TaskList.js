72aff9508ad7140553963eb1e02f4bc2e5d4f037
Rain Tomista <rftomista@gmail.com>
2022-04-19 18:33:07 +0800

    Remove TaskList props

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { useDispatch, useSelector } from "react-redux";
 import { updateTaskState } from "../lib/store";
 import Task from "./Task";
@@ -81,18 +80,3 @@ export default function TaskList() {
     </div>
   );
 }
-
-TaskList.propTypes = {
-  /** Checks if it's in loading state */
-  loading: PropTypes.bool,
-  /** The list of tasks */
-  tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
-  /** Event to change the task to pinned */
-  onPinTask: PropTypes.func,
-  /** Event to change the task to archived */
-  onArchiveTask: PropTypes.func,
-};
-
-TaskList.defaultProps = {
-  loading: false,
-};
